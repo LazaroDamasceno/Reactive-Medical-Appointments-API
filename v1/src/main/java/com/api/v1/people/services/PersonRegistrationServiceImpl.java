fc130fc1b4ebc3f89e2ac240e046a9a96f4fffc7
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-15 21:42:06 -0300

    Changed PersonRegistrationServiceImpl

--- v1/src/main/java/com/api/v1/people/services/PersonRegistrationServiceImpl.java
+++ v1/src/main/java/com/api/v1/people/services/PersonRegistrationServiceImpl.java
@@ -3,6 +3,8 @@ package com.api.v1.people.services;
 import com.api.v1.db.DbSets;
 import com.api.v1.people.domain.Person;
 import com.api.v1.people.dtos.PersonRegistrationDto;
+import com.api.v1.people.exceptions.DuplicatedEmailException;
+import com.api.v1.people.exceptions.DuplicatedSsnException;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -13,11 +15,33 @@ class PersonRegistrationServiceImpl implements PersonRegistrationService {
     @Override
     public Mono<Person> register(@Valid PersonRegistrationDto registrationDto) {
         return Mono.defer(() -> {
-            Person person = Person.create(registrationDto);
-            DbSets.peopleCollection()
-                    .document()
-                    .set(person);
-            return Mono.just(person);
+            try {
+                boolean isSsnDuplicated = !DbSets
+                        .peopleCollection()
+                        .whereEqualTo("ssn", registrationDto.ssn())
+                        .get()
+                        .get()
+                        .isEmpty();
+                if (isSsnDuplicated) {
+                    return Mono.error(DuplicatedSsnException::new);
+                }
+                boolean isEmailDuplicated = !DbSets
+                        .peopleCollection()
+                        .whereEqualTo("email", registrationDto.email())
+                        .get()
+                        .get()
+                        .isEmpty();
+                if (isEmailDuplicated) {
+                    return Mono.error(DuplicatedEmailException::new);
+                }
+                Person person = Person.create(registrationDto);
+                DbSets.peopleCollection()
+                        .document()
+                        .set(person);
+                return Mono.just(person);
+            } catch (Exception ignored) {
+                return Mono.empty();
+            }
         });
     }
 
