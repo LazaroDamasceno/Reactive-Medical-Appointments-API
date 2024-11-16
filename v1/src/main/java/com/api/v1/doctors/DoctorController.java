d766abfc5fe250714f12aed31d050c7e6494d537
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-15 23:24:24 -0300

    Created the controller DoctorController

---
+++ v1/src/main/java/com/api/v1/doctors/DoctorController.java
@@ -0,0 +1,27 @@
+package com.api.v1.doctors;
+
+import com.api.v1.doctors.dtos.DoctorRegistrationDto;
+import com.api.v1.doctors.dtos.DoctorResponseDto;
+import com.api.v1.doctors.services.DoctorRegistrationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("api/v1/doctors")
+@RequiredArgsConstructor
+public class DoctorController {
+
+    private final DoctorRegistrationService registrationService;
+
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Mono<DoctorResponseDto> register(@RequestBody @Valid DoctorRegistrationDto registrationDto) {
+        return registrationService.register(registrationDto );
+    }
+
+
+
+}
