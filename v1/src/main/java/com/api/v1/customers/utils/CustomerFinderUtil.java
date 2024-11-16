cb226134862fbb42ed49887b91292e3489e3bef0
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-16 18:20:31 -0300

    Created the util CustomerFinderUtil

---
+++ v1/src/main/java/com/api/v1/customers/utils/CustomerFinderUtil.java
@@ -0,0 +1,30 @@
+package com.api.v1.customers.utils;
+
+import com.api.v1.customers.CustomerNotFoundException;
+import com.api.v1.customers.domain.Customer;
+import com.api.v1.customers.domain.CustomerRepository;
+import com.api.v1.people.annotations.SSN;
+import com.api.v1.people.utils.PersonFinderUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class CustomerFinderUtil {
+
+    private final PersonFinderUtil personFinderUtil;
+    private final CustomerRepository customerRepository;
+
+    public Mono<Customer> find(@SSN String ssn) {
+        return personFinderUtil
+                .find(ssn)
+                .flatMap(foundPerson -> {
+                    if (foundPerson == null) return Mono.error(CustomerNotFoundException::new);
+                    return customerRepository
+                            .findByPerson(foundPerson)
+                            .flatMap(Mono::just);
+                });
+    }
+
+}
