74115f79158ef1c9ddc20c779ab99186a88c8e26
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-16 16:25:05 -0300

    Created the util CustomerResponseMapper

---
+++ v1/src/main/java/com/api/v1/customers/utils/CustomerResponseMapper.java
@@ -0,0 +1,18 @@
+package com.api.v1.customers.utils;
+
+import com.api.v1.customers.domain.Customer;
+import com.api.v1.customers.dtos.CustomerResponseDto;
+import com.api.v1.people.utils.PersonResponseMapper;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class CustomerResponseMapper {
+
+    public CustomerResponseDto map(Customer customer) {
+        return new CustomerResponseDto(
+                PersonResponseMapper.map(customer.getPerson()),
+                customer.getCreatedAt()
+        );
+    }
+
+}
