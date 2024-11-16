c7a7055b45d345ae2808931cd313743e0e6b63d5
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-16 13:14:42 -0300

    Replaced Firebase with MongoDB

--- v1/src/main/java/com/api/v1/customers/dtos/CustomerResponseDto.java
+++
@@ -1,9 +0,0 @@
-package com.api.v1.customers.dtos;
-
-import com.api.v1.people.domain.Person;
-
-public record CustomerResponseDto(
-        Person person,
-        String createdAt
-) {
-}
--- v1/src/main/java/com/api/v1/people/dtos/AddressDto.java
+++
@@ -1,9 +0,0 @@
-package com.api.v1.people.dtos;
-
-public record AddressDto(
-        String state,
-        String city,
-        String street,
-        String zipcode
-) {
-}
