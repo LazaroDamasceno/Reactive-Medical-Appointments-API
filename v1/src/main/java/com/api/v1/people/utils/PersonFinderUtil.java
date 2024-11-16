2e64f12da0bf14a98e155d3a1032d96d06472c34
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-16 18:16:44 -0300

    Created the util PersonFinderUtil

---
+++ v1/src/main/java/com/api/v1/people/utils/PersonFinderUtil.java
@@ -0,0 +1,22 @@
+package com.api.v1.people.utils;
+
+import com.api.v1.people.annotations.SSN;
+import com.api.v1.people.domain.Person;
+import com.api.v1.people.domain.PersonRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class PersonFinderUtil {
+
+    private final PersonRepository personRepository;
+
+    public Mono<Person> find(@SSN String ssn) {
+        return personRepository
+                .findBySsn(ssn)
+                .flatMap( Mono::just);
+    }
+
+}
