1e50467bb2e835e028e91e580a16f72bf86562c3
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-15 21:49:27 -0300

    Created the util PersonFinderUtil

---
+++ v1/src/main/java/com/api/v1/people/utils/PersonFinderUtil.java
@@ -0,0 +1,29 @@
+package com.api.v1.people.utils;
+
+import com.api.v1.db.DbSets;
+import com.api.v1.people.domain.Person;
+import lombok.experimental.UtilityClass;
+import reactor.core.publisher.Mono;
+
+@UtilityClass
+public class PersonFinderUtil {
+
+    public Mono<Person> find(String ssn) {
+        return Mono.defer(() -> {
+            try {
+                Person foundPerson = DbSets
+                        .peopleCollection()
+                        .whereEqualTo("ssn", ssn)
+                        .get()
+                        .get()
+                        .getDocuments()
+                        .getFirst()
+                        .toObject(Person.class);
+                return Mono.just(foundPerson);
+            } catch (Exception ignored) {
+                return Mono.empty();
+            }
+        });
+    }
+
+}
