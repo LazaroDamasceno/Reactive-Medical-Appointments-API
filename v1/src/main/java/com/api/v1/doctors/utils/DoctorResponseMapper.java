b5567e6b41ca754fb2f5697468fa649d137ceea2
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-16 19:41:24 -0300

    Created the util DoctorResponseMapper

---
+++ v1/src/main/java/com/api/v1/doctors/utils/DoctorResponseMapper.java
@@ -0,0 +1,20 @@
+package com.api.v1.doctors.utils;
+
+import com.api.v1.doctors.domain.Doctor;
+import com.api.v1.doctors.dtos.DoctorResponseDto;
+import com.api.v1.people.utils.PersonResponseMapper;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class DoctorResponseMapper {
+
+    public DoctorResponseDto map(Doctor doctor) {
+        return new DoctorResponseDto(
+                doctor.getLicenseNumber(),
+                PersonResponseMapper.map(doctor.getPerson()),
+                doctor.getHiredAt(),
+                doctor.getTerminatedAt()
+        );
+    }
+
+}
