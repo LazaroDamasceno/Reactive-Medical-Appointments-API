5c8437b74bb7d52308dc2069f95842509fc61f6a
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-17 20:38:03 -0300

    Created the test class CustomerModificationTest

---
+++ v1/src/test/java/com/api/v1/CustomerModificationTest.java
@@ -0,0 +1,67 @@
+package com.api.v1;
+
+import com.api.v1.doctors.dtos.DoctorLicenseNumberDto;
+import com.api.v1.doctors.dtos.DoctorModificationDto;
+import com.api.v1.people.dtos.AddressDto;
+import com.api.v1.people.dtos.FullNameDto;
+import com.api.v1.people.dtos.PersonModificationDto;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.LocalDate;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CustomerModificationTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    PersonModificationDto modificationDto = new PersonModificationDto(
+            new FullNameDto(
+                    "Leonardo",
+                    "Silva",
+                    "Santos Jr."
+            ),
+            LocalDate.parse("2004-12-12"),
+            "jr@leosantos.com",
+            new AddressDto(
+                    "CO",
+                    "Denver",
+                    "Downtown",
+                    "90012"
+            ),
+            "1234567890",
+            "cis male"
+    );
+
+    @Test
+    @Order(1)
+    void testSuccessful() {
+        String ssn = "123456789";
+        webTestClient
+                .put()
+                .uri("api/v1/customers/%s".formatted(ssn))
+                .bodyValue(modificationDto)
+                .exchange()
+                .expectStatus().is2xxSuccessful();
+    }
+
+    @Test
+    @Order(2)
+    void testUnsuccessful() {
+        String ssn = "123456779";
+        webTestClient
+                .put()
+                .uri("api/v1/customers/%s".formatted(ssn))
+                .bodyValue(modificationDto)
+                .exchange()
+                .expectStatus().is5xxServerError();
+    }
+
+}
