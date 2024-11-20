908d52cabb51921f4ef358f40d1b0fa9dbe6833e
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-20 03:00:03 -0300

    Created the repository CardRepository

---
+++ v1/src/main/java/com/api/v1/cards/domain/CardRepository.java
@@ -0,0 +1,15 @@
+package com.api.v1.cards.domain;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+public interface CardRepository extends ReactiveMongoRepository<Card, UUID> {
+
+    @Query("{ 'number': ?0 }")
+    Mono<Card> findByNumber(ObjectId number);
+
+}
