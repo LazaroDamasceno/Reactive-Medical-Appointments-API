520ecc6bffc2cb41579cc920cab1866e5d29553e
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-21 00:56:35 -0300

    Deleted the folder Card

--- v1/src/main/java/com/api/v1/cards/exceptions/CardNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.api.v1.cards.exceptions;
-
-public class CardNotFoundException extends RuntimeException {
-    public CardNotFoundException() {
-        super("Card was not found.");
-    }
-}
