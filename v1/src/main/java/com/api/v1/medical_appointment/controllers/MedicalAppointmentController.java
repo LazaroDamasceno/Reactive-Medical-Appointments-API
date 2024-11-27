7268201c9979c70678ca87b1da3e73d4d55f57be
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-27 18:06:31 -0300

    Added to MedicalAppointmentController the methods cancel(String orderNumber) and modify(String orderNumber)

--- v1/src/main/java/com/api/v1/medical_appointment/controllers/MedicalAppointmentController.java
+++ v1/src/main/java/com/api/v1/medical_appointment/controllers/MedicalAppointmentController.java
@@ -1,15 +1,14 @@
 package com.api.v1.medical_appointment.controllers;
 
+import com.api.v1.medical_appointment.annotation.OrderNumber;
 import com.api.v1.medical_appointment.domain.MedicalAppointment;
-import com.api.v1.medical_appointment.domain.MedicalAppointmentRepository;
 import com.api.v1.medical_appointment.dtos.MedicalAppointmentBookingDto;
 import com.api.v1.medical_appointment.services.MedicalAppointmentBookingService;
+import com.api.v1.medical_appointment.services.MedicalAppointmentCancellationService;
+import com.api.v1.medical_appointment.services.MedicalAppointmentCompletionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -18,9 +17,21 @@ import reactor.core.publisher.Mono;
 public class MedicalAppointmentController {
 
     private final MedicalAppointmentBookingService bookingService;
+    private final MedicalAppointmentCancellationService cancellationService;
+    private final MedicalAppointmentCompletionService completionService;
 
     @PostMapping
     public Mono<MedicalAppointment> book(@Valid @RequestBody MedicalAppointmentBookingDto bookingDto) {
         return bookingService.book(bookingDto);
+    }
+
+    @PatchMapping("{orderNumber}/cancellation")
+    public Mono<Void> cancel(@OrderNumber @PathVariable String orderNumber) {
+        return cancellationService.cancel(orderNumber);
+    }
+
+    @PatchMapping("{orderNumber}/completion")
+    public Mono<Void> complete(@OrderNumber @PathVariable String orderNumber) {
+        return completionService.complete(orderNumber);
     }
 }
