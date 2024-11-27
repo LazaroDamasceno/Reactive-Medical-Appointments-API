9cb148c40248281e1d062607fb070676b1afebd3
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-27 13:19:29 -0300

    Created the test class DoctorTerminationTest

--- v1/src/main/java/com/api/v1/doctors/dtos/DoctorRegistrationDto.java
+++ v1/src/main/java/com/api/v1/doctors/dtos/DoctorRegistrationDto.java
@@ -4,7 +4,7 @@ import com.api.v1.people.dtos.PersonRegistrationDto;
 import jakarta.validation.Valid;
 
 public record DoctorRegistrationDto(
-        @Valid DoctorLicenseNumberDto licenseNumberDto,
+        @Valid String licenseNumberDto,
         @Valid PersonRegistrationDto personRegistrationDto
 ) {
 }
---
+++ v1/src/test/java/com/api/v1/DoctorTerminationTest.java
@@ -0,0 +1,51 @@
+package com.api.v1;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class DoctorTerminationTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    @Order(1)
+    void testSuccessful() {
+        String medicalLicenseNumber = "12345678CA";
+        webTestClient
+                .patch()
+                .uri("api/v1/doctors/%s".formatted(medicalLicenseNumber))
+                .exchange()
+                .expectStatus().is2xxSuccessful();
+    }
+
+    @Test
+    @Order(2)
+    void testUnsuccessful1() {
+        String medicalLicenseNumber = "12345678CA";
+        webTestClient
+                .patch()
+                .uri("api/v1/doctors/%s".formatted(medicalLicenseNumber))
+                .exchange()
+                .expectStatus().is5xxServerError();
+    }
+
+    @Test
+    @Order(2)
+    void testUnsuccessful2() {
+        String medicalLicenseNumber = "12345677CA";
+        webTestClient
+                .patch()
+                .uri("api/v1/doctors/%s".formatted(medicalLicenseNumber))
+                .exchange()
+                .expectStatus().is5xxServerError();
+    }
+
+}
