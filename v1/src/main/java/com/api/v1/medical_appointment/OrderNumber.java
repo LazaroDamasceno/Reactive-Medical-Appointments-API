b5b9b30eec5a925d16f0a74e2d72ff0332ab5f7d
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-27 14:20:46 -0300

    Created the annotation OrderNumber

---
+++ v1/src/main/java/com/api/v1/medical_appointment/OrderNumber.java
@@ -0,0 +1,18 @@
+package com.api.v1.medical_appointment;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+@NotNull
+@Size(min = 12, max = 12)
+@Pattern(regexp = "^\\d{12}")
+public @interface OrderNumber {
+}
