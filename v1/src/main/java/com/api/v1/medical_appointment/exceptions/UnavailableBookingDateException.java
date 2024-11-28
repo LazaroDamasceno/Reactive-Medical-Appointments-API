de1f53a8eb2b1bb38c85bf31800acb43268d7398
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-27 23:15:26 -0300

    Revert "Created the exception UnavailableBookingDateException"
    This reverts commit fd4b667d98c9ae7ad953b2638b0c047670679fb1.

--- v1/src/main/java/com/api/v1/medical_appointment/exceptions/UnavailableBookingDateException.java
+++
@@ -1,7 +0,0 @@
-package com.api.v1.medical_appointment.exceptions;
-
-public class UnavailableBookingDateException extends RuntimeException {
-    public UnavailableBookingDateException(String message) {
-        super(message);
-    }
-}
