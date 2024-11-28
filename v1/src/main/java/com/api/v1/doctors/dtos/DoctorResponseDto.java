3cd6ae0ab98fc8f506ad8c71e58c1a5f04b088e5
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-28 02:07:49 -0300

    Reapply "Fixed all the response DTOs and their maps"
    This reverts commit ea32d7ab76eca2b648ad9fa544361176298613f2.

--- v1/src/main/java/com/api/v1/doctors/dtos/DoctorResponseDto.java
+++ v1/src/main/java/com/api/v1/doctors/dtos/DoctorResponseDto.java
@@ -1,15 +1,12 @@
 package com.api.v1.doctors.dtos;
 
 import com.api.v1.people.dtos.PersonResponseDto;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.util.Date;
 
 public record DoctorResponseDto(
         String licenseNumber,
         PersonResponseDto personResponseDto,
-        LocalDateTime hiredAt,
-        ZoneId hiredAtAt,
-        LocalDateTime terminatedAt,
-        ZoneId terminatedAtZone
+        Date hiredAt,
+        Date terminatedAt
 ) {
 }
