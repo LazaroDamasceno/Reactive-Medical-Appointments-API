5ec52eed6b071431e7d7774fcadb19b5cdc6987b
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-29 22:29:22 -0300

    Created the test class MedicalAppointmentCompletionTest

---
+++ v1/src/test/java/com/api/v1/medical_appointments/MedicalAppointmentCompletionTest.java
@@ -0,0 +1,50 @@
+package com.api.v1.medical_appointments;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class MedicalAppointmentCompletionTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    String orderNumber = "";
+
+    @Test
+    @Order(1)
+    void testSuccessful() {
+        webTestClient
+                .patch()
+                .uri("api/v1/medical-appointments/%s/cancellation".formatted(orderNumber))
+                .exchange()
+                .expectStatus().is2xxSuccessful();
+    }
+
+    @Test
+    @Order(2)
+    void testUnSuccessfulForImmutableMedicalAppointment() {
+        webTestClient
+                .patch()
+                .uri("api/v1/medical-appointments/%s/cancellation".formatted(orderNumber))
+                .exchange()
+                .expectStatus().is5xxServerError();
+    }
+
+    @Test
+    @Order(3)
+    void testUnSuccessfulForNotFoundMedicalAppointment() {
+        webTestClient
+                .patch()
+                .uri("api/v1/medical-appointments/%s/cancellation".formatted("123456789012345678901234"))
+                .exchange()
+                .expectStatus().is5xxServerError();
+    }
+
+}
