4bc8c08e735c38af6bfefcddb985e0bf9c45c3af
lazarodamasceno <lazarolawrence2@gmail.co>
2024-11-29 23:17:22 -0300

    Created the mapper PaymentResponseMapper

---
+++ v1/src/main/java/com/api/v1/payment/PaymentResponseMapper.java
@@ -0,0 +1,21 @@
+package com.api.v1.payment;
+
+import com.api.v1.cards.utils.CardResponseMapper;
+import com.api.v1.payment.domain.Payment;
+import com.api.v1.payment.dtos.PaymentResponseDto;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+
+public class PaymentResponseMapper {
+
+    public static PaymentResponseDto mapToDto(Payment payment) {
+        return new PaymentResponseDto(
+                payment.number(),
+                CardResponseMapper.mapToDto(payment.card()),
+                payment.price(),
+                ZonedDateTime.of(LocalDateTime.parse(payment.createdAt()), payment.createdAtZone())
+        );
+    }
+
+}
