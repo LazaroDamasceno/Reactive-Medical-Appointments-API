84ad3033fb1b33b779cedf2874daababddff645c
lazarodamasceno <lazarolawrence2@gmail.co>
2024-12-02 23:59:52 -0300

    Changed PaymentController and CardController

--- v1/src/main/java/com/api/v1/cards/controllers/CardController.java
+++ v1/src/main/java/com/api/v1/cards/controllers/CardController.java
@@ -2,12 +2,14 @@ package com.api.v1.cards.controllers;
 
 import com.api.v1.cards.domain.Card;
 import com.api.v1.cards.dtos.CardRegistrationDto;
+import com.api.v1.cards.dtos.CardResponseDto;
 import com.api.v1.cards.services.CardDeletionService;
 import com.api.v1.cards.services.CardRegistrationService;
 import com.api.v1.cards.services.CardRetrievalService;
 import com.api.v1.medical_appointments.annotation.OrderNumber;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -22,36 +24,43 @@ public class CardController {
     private final CardRetrievalService retrievalService;
 
     @PostMapping("credit-card")
-    public Mono<Card> registerCreditCard(@Valid @RequestBody CardRegistrationDto registrationDto) {
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Mono<CardResponseDto> registerCreditCard(@Valid @RequestBody CardRegistrationDto registrationDto) {
         return registrationService.registerCreditCard(registrationDto);
     }
 
     @PostMapping("debit-card")
-    public Mono<Card> registerDebitCard(@Valid @RequestBody CardRegistrationDto registrationDto) {
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Mono<CardResponseDto> registerDebitCard(@Valid @RequestBody CardRegistrationDto registrationDto) {
         return registrationService.registerDebitCard(registrationDto);
     }
 
     @DeleteMapping("{cardNumber}")
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public Mono<Void> deleteByCardNumber(@PathVariable String cardNumber) {
         return deletionService.deleteByCardNumber(cardNumber);
     }
 
     @GetMapping("{cardNumber}")
+    @ResponseStatus(value = HttpStatus.OK)
     public Mono<Card> findByNumber(@OrderNumber @ PathVariable String cardNumber) {
         return retrievalService.findByNumber(cardNumber);
     }
 
     @GetMapping
+    @ResponseStatus(value = HttpStatus.OK)
     public Flux<Card> findAll() {
         return retrievalService.findAll();
     }
 
     @GetMapping("credit-cards")
+    @ResponseStatus(value = HttpStatus.OK)
     public Flux<Card> findAllCreditCards() {
         return retrievalService.findAllCreditCards();
     }
 
     @GetMapping("debit-cards")
+    @ResponseStatus(value = HttpStatus.OK)
     public Flux<Card> findAllDebitCards() {
         return retrievalService.findAllDebitCards();
     }
--- v1/src/main/java/com/api/v1/payment/controllers/PaymentController.java
+++ v1/src/main/java/com/api/v1/payment/controllers/PaymentController.java
@@ -5,18 +5,19 @@ import com.api.v1.payment.dtos.PaymentResponseDto;
 import com.api.v1.payment.services.MedicalAppointmentPaymentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("api/v1/payments")
 @RequiredArgsConstructor
-public class PaymentController implements MedicalAppointmentPaymentService {
+public class PaymentController {
 
     private final MedicalAppointmentPaymentService paymentPaymentService;
 
-    @Override
     @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
     public Mono<PaymentResponseDto> payMedicalAppointment(@Valid @RequestBody MedicalAppointmentPaymentDto paymentDto) {
         return paymentPaymentService.payMedicalAppointment(paymentDto);
     }
