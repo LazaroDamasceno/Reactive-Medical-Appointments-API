e0b5997dc6efd60c7b4664a29f4dd71097ca5174
lazarodamasceno <lazarolawrence2@gmail.co>
2024-12-04 22:32:34 -0300

    Created the test class PaymentMedicalPaymentTest

---
+++ v1/src/test/java/com/api/v1/payments/PaymentMedicalPaymentTest.java
@@ -0,0 +1,64 @@
+package com.api.v1.payments;
+
+import com.api.v1.payment.dtos.MedicalAppointmentPaymentDto;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PaymentMedicalPaymentTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    @Order(1)
+    public void testSuccessful() {
+        var paymentDto = new MedicalAppointmentPaymentDto(
+                "",
+                "",
+                80.0
+        );
+        webTestClient.post()
+                .uri("api/v1/payments")
+                .bodyValue(paymentDto)
+                .exchange()
+                .expectStatus().is2xxSuccessful();
+    }
+
+    @Test
+    @Order(2)
+    public void testUnsuccessful() {
+        var paymentDto = new MedicalAppointmentPaymentDto(
+                "",
+                "",
+                80.0
+        );
+        webTestClient.post()
+                .uri("api/v1/payments")
+                .bodyValue(paymentDto)
+                .exchange()
+                .expectStatus().is5xxServerError();
+    }
+
+    @Test
+    @Order(3)
+    public void testUnsuccessful2() {
+        var paymentDto = new MedicalAppointmentPaymentDto(
+                "",
+                "",
+                80.0
+        );
+        webTestClient.post()
+                .uri("api/v1/payments")
+                .bodyValue(paymentDto)
+                .exchange()
+                .expectStatus().is5xxServerError();
+    }
+
+}
