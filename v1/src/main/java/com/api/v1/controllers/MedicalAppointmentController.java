8ac0c1a0a8ccf817145a94bc2802d64ab18b1244
lazarodamasceno <lazarolawrence2@gmail.co>
2024-12-11 01:40:28 -0300

    Refactored MedicalAppointmentController

--- v1/src/main/java/com/api/v1/controllers/MedicalAppointmentController.java
+++ v1/src/main/java/com/api/v1/controllers/MedicalAppointmentController.java
@@ -1,6 +1,8 @@
 package com.api.v1.controllers;
 
+import com.api.v1.annotations.MedicalLicenseNumber;
 import com.api.v1.annotations.OrderNumber;
+import com.api.v1.annotations.SSN;
 import com.api.v1.dtos.medical_appointments.MedicalAppointmentBookingDto;
 import com.api.v1.dtos.medical_appointments.MedicalAppointmentResponseDto;
 import com.api.v1.services.medical_appointments.MedicalAppointmentBookingService;
@@ -20,18 +22,18 @@ public class MedicalAppointmentController {
     private final MedicalAppointmentBookingService bookingService;
     private final MedicalAppointmentCancellationService cancellationService;
     private final MedicalAppointmentCompletionService completionService;
-    //private final MedicalAppointmentRetrievalService retrievalService;
+    private final MedicalAppointmentRetrievalService retrievalService;
 
     public MedicalAppointmentController(
             MedicalAppointmentBookingService bookingService,
             MedicalAppointmentCancellationService cancellationService,
-            MedicalAppointmentCompletionService completionService
-            //MedicalAppointmentRetrievalService retrievalService
+            MedicalAppointmentCompletionService completionService,
+            MedicalAppointmentRetrievalService retrievalService
     ) {
         this.bookingService = bookingService;
         this.cancellationService = cancellationService;
         this.completionService = completionService;
-        //this.retrievalService = retrievalService;
+        this.retrievalService = retrievalService;
     }
 
     public Mono<MedicalAppointmentResponseDto> book(@Valid @RequestBody MedicalAppointmentBookingDto bookingDto) {
@@ -74,4 +76,98 @@ public class MedicalAppointmentController {
         return completionService.complete(orderNumber);
     }
 
+    @GetMapping("{orderNumber}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Mono<MedicalAppointmentResponseDto> findByOrderNumber(@PathVariable @OrderNumber String orderNumber) {
+        return retrievalService.findByOrderNumber(orderNumber);
+    }
+
+    @GetMapping
+    @ResponseStatus(value = HttpStatus.OK)
+    public Flux<MedicalAppointmentResponseDto> findAll() {
+        return retrievalService.findAll();
+    }
+
+    @GetMapping("{ssn}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Flux<MedicalAppointmentResponseDto> findAllByCustomer(@PathVariable @SSN String ssn) {
+        return retrievalService.findActiveCustomer(ssn);
+    }
+
+    @GetMapping("{medicalLicenseNumber}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Flux<MedicalAppointmentResponseDto> findAllByDoctor(
+            @PathVariable @MedicalLicenseNumber String medicalLicenseNumber
+    ) {
+        return retrievalService.findAllByDoctor(medicalLicenseNumber);
+    }
+
+    @GetMapping("{ssn}/{medicalLicenseNumber}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Flux<MedicalAppointmentResponseDto> findAllByCustomerAndDoctor(
+            @PathVariable @SSN String ssn,
+            @PathVariable @MedicalLicenseNumber String medicalLicenseNumber
+    ) {
+        return retrievalService.findAllByCustomerAndDoctor(ssn, medicalLicenseNumber);
+    }
+
+    @GetMapping("active/{ssn}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Flux<MedicalAppointmentResponseDto> findActiveCustomer(@PathVariable @SSN String ssn) {
+        return retrievalService.findActiveCustomer(ssn);
+    }
+
+    @GetMapping("active/{medicalLicenseNumber}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Flux<MedicalAppointmentResponseDto> findActiveByDoctor(
+            @PathVariable @MedicalLicenseNumber String medicalLicenseNumber
+    ) {
+        return retrievalService.findActiveByDoctor(medicalLicenseNumber);
+    }
+
+    @GetMapping("active/{ssn}/{medicalLicenseNumber}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Flux<MedicalAppointmentResponseDto> findActiveByCustomerAndDoctor(
+            @PathVariable @SSN String ssn,
+            @PathVariable @MedicalLicenseNumber String medicalLicenseNumber
+    ) {
+        return retrievalService.findActiveByCustomerAndDoctor(ssn, medicalLicenseNumber);
+    }
+
+    @GetMapping("completed/{ssn}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Flux<MedicalAppointmentResponseDto> findCompletedCustomer(@PathVariable @SSN String ssn) {
+        return retrievalService.findCompletedCustomer(ssn);
+    }
+
+    @GetMapping("completed/{medicalLicenseNumber}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Flux<MedicalAppointmentResponseDto> findCompletedByDoctor(
+            @PathVariable @MedicalLicenseNumber String medicalLicenseNumber
+    ) {
+        return retrievalService.findCompletedByDoctor(medicalLicenseNumber);
+    }
+
+    @GetMapping("completed/{ssn}/{medicalLicenseNumber}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Flux<MedicalAppointmentResponseDto> findCompletedByCustomerAndDoctor(
+            @PathVariable @SSN String ssn,
+            @PathVariable @MedicalLicenseNumber String medicalLicenseNumber
+    ) {
+        return retrievalService.findCompletedByCustomerAndDoctor(ssn, medicalLicenseNumber);
+    }
+
+    @GetMapping("canceled/{ssn}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Flux<MedicalAppointmentResponseDto> findCanceledCustomer(@PathVariable @SSN String ssn) {
+        return retrievalService.findCanceledCustomer(ssn);
+    }
+
+    @GetMapping("canceled/{findCanceledByDoctor}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Flux<MedicalAppointmentResponseDto> findCanceledByDoctor(
+            @PathVariable @MedicalLicenseNumber String medicalLicenseNumber
+    ) {
+        return retrievalService.findCanceledByDoctor(medicalLicenseNumber);
+    }
 }
