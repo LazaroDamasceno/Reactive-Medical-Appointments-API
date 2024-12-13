2f112442506a1f8fe955f579e5907826ac417415
lazarodamasceno <lazarolawrence2@gmail.co>
2024-12-12 23:47:28 -0300

    Changed the service MedicalAppointmentBookingServiceImpl

--- v1/src/main/java/com/api/v1/domain/medical_appointments/MedicalAppointmentRepository.java
+++ v1/src/main/java/com/api/v1/domain/medical_appointments/MedicalAppointmentRepository.java
@@ -1,7 +1,5 @@
 package com.api.v1.domain.medical_appointments;
 
-import com.api.v1.domain.customers.Customer;
-import com.api.v1.domain.doctors.Doctor;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
@@ -11,14 +9,5 @@ public interface MedicalAppointmentRepository extends ReactiveMongoRepository<Me
 
     @Query("{ 'orderNumber': ?0 }")
     Mono<MedicalAppointment> findByOrderNumber(ObjectId orderNumber);
-
-    @Query("""
-            { 'customer': ?0 },
-            { 'doctor': ?1 },
-            { 'bookedAt': ?2 },
-            { 'canceledAt': null },
-            { 'completedAt': null }
-    """)
-    Mono<MedicalAppointment> find(Customer customer, Doctor doctor, String bookedAt);
 
 }
--- v1/src/main/java/com/api/v1/services/impl/MedicalAppointmentBookingServiceImpl.java
+++ v1/src/main/java/com/api/v1/services/impl/MedicalAppointmentBookingServiceImpl.java
@@ -9,7 +9,6 @@ import com.api.v1.domain.medical_appointments.MedicalAppointment;
 import com.api.v1.domain.medical_appointments.MedicalAppointmentRepository;
 import com.api.v1.dtos.medical_appointments.MedicalAppointmentBookingDto;
 import com.api.v1.dtos.medical_appointments.MedicalAppointmentResponseDto;
-import com.api.v1.exceptions.medical_appointments.UnavailableSlotException;
 import com.api.v1.utils.medical_appointments.MedicalAppointmentResponseMapper;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
@@ -39,23 +38,17 @@ public class MedicalAppointmentBookingServiceImpl implements MedicalAppointmentB
         return customerMono
                 .zipWith(doctorMono)
                 .flatMap(tuple -> {
-                    return medicalAppointmentRepository
-                            .find(tuple.getT1(), tuple.getT2(), bookingDto.bookingDate().toString())
-                            .singleOptional()
-                            .flatMap(optional -> {
-                               if (optional.isPresent()) {
-                                   return Mono.error(new UnavailableSlotException(bookingDto.bookingDate(), bookingDto.medicalLicenseNumber()));
-                               }
-                               return Mono.defer(() -> {
-                                  MedicalAppointment medicalAppointment = MedicalAppointment.create(
-                                          tuple.getT1(),
-                                          tuple.getT2(),
-                                          bookingDto.bookingDate(),
-                                          "Medical appointment covered by the customer"
-                                  );
-                                  return medicalAppointmentRepository.save(medicalAppointment);
-                               });
-                            });
+                    return Mono.defer(() -> {
+                        Customer customer = tuple.getT1();
+                        Doctor doctor = tuple.getT2();
+                        MedicalAppointment medicalAppointment = MedicalAppointment.create(
+                                customer,
+                                doctor,
+                                bookingDto.bookingDate(),
+                                "Medical appointment covered by the customer."
+                        );
+                        return medicalAppointmentRepository.save(medicalAppointment);
+                    });
                 })
                 .flatMap(MedicalAppointmentResponseMapper::mapToMono);
     }
@@ -66,50 +59,40 @@ public class MedicalAppointmentBookingServiceImpl implements MedicalAppointmentB
         Mono<Doctor> doctorMono = doctorFinderUtil.find(bookingDto.medicalLicenseNumber());
         return customerMono
                 .zipWith(doctorMono)
-                .flatMap(tuple -> medicalAppointmentRepository
-                        .find(tuple.getT1(), tuple.getT2(), bookingDto.bookingDate().toString())
-                        .singleOptional()
-                        .flatMap(optional -> {
-                            if (optional.isPresent()) {
-                                return Mono.error(new UnavailableSlotException(bookingDto.bookingDate(), bookingDto.medicalLicenseNumber()));
-                            }
-                            return Mono.defer(() -> {
-                                MedicalAppointment medicalAppointment = MedicalAppointment.create(
-                                        tuple.getT1(),
-                                        tuple.getT2(),
-                                        bookingDto.bookingDate(),
-                                        "Medical appointment covered by the Affordable Care Act"
-                                );
-                                return medicalAppointmentRepository.save(medicalAppointment);
-                            });
-                }))
+                .flatMap(tuple -> {
+                    return Mono.defer(() -> {
+                        Customer customer = tuple.getT1();
+                        Doctor doctor = tuple.getT2();
+                        MedicalAppointment medicalAppointment = MedicalAppointment.create(
+                                customer,
+                                doctor,
+                                bookingDto.bookingDate(),
+                                "Medical appointment covered by the Affordable Care Act.."
+                        );
+                        return medicalAppointmentRepository.save(medicalAppointment);
+                    });
+                })
                 .flatMap(MedicalAppointmentResponseMapper::mapToMono);
     }
 
     @Override
-    public Mono<MedicalAppointmentResponseDto> bookPrivateHeathCareMedicalAppointment(MedicalAppointmentBookingDto bookingDto) {
+    public Mono<MedicalAppointmentResponseDto> bookPrivateHeathCareMedicalAppointment(@Valid MedicalAppointmentBookingDto bookingDto) {
         Mono<Customer> customerMono = customerFinderUtil.find(bookingDto.ssn());
         Mono<Doctor> doctorMono = doctorFinderUtil.find(bookingDto.medicalLicenseNumber());
         return customerMono
                 .zipWith(doctorMono)
                 .flatMap(tuple -> {
-                    return medicalAppointmentRepository
-                            .find(tuple.getT1(), tuple.getT2(), bookingDto.bookingDate().toString())
-                            .singleOptional()
-                            .flatMap(optional -> {
-                                if (optional.isPresent()) {
-                                    return Mono.error(new UnavailableSlotException(bookingDto.bookingDate(), bookingDto.medicalLicenseNumber()));
-                                }
-                                return Mono.defer(() -> {
-                                    MedicalAppointment medicalAppointment = MedicalAppointment.create(
-                                            tuple.getT1(),
-                                            tuple.getT2(),
-                                            bookingDto.bookingDate(),
-                                            "Medical appointment covered by the private health care"
-                                    );
-                                    return medicalAppointmentRepository.save(medicalAppointment);
-                                });
-                            });
+                    return Mono.defer(() -> {
+                        Customer customer = tuple.getT1();
+                        Doctor doctor = tuple.getT2();
+                        MedicalAppointment medicalAppointment = MedicalAppointment.create(
+                                customer,
+                                doctor,
+                                bookingDto.bookingDate(),
+                                "Medical appointment covered by the private health care."
+                        );
+                        return medicalAppointmentRepository.save(medicalAppointment);
+                    });
                 })
                 .flatMap(MedicalAppointmentResponseMapper::mapToMono);
     }
