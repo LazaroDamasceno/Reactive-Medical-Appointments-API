031f1c8c992efd3d6818671c7122780540f6978a
lazarodamasceno <lazarolawrence2@gmail.co>
2024-12-14 03:34:48 -0300

    Refactored MedicalSlotCancellationServiceImpl

--- v1/src/main/java/com/api/v1/services/impl/MedicalSlotCancellationServiceImpl.java
+++ v1/src/main/java/com/api/v1/services/impl/MedicalSlotCancellationServiceImpl.java
@@ -1,6 +1,7 @@
 package com.api.v1.services.impl;
 
 import com.api.v1.annotations.MongoDbId;
+import com.api.v1.domain.medical_slots.MedicalSlot;
 import com.api.v1.domain.medical_slots.MedicalSlotRepository;
 import com.api.v1.exceptions.medical_slots.ImmutableMedicalSlotException;
 import com.api.v1.services.medical_slots.MedicalSlotCancellationService;
@@ -8,8 +9,6 @@ import com.api.v1.utils.medical_slots.MedicalSlotFinderUtil;
 import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
-
-import java.util.concurrent.atomic.AtomicReference;
 
 @Service
 public class MedicalSlotCancellationServiceImpl implements MedicalSlotCancellationService {
@@ -29,18 +28,30 @@ public class MedicalSlotCancellationServiceImpl implements MedicalSlotCancellati
     public Mono<Void> cancel(@MongoDbId String id) {
         return medicalSlotFinderUtil
                 .find(new ObjectId(id))
-                .flatMap(slot -> {
-                    AtomicReference<String> message = new AtomicReference<>("Medical slot is already canceled.");
-                    if (slot.getCanceledAt() != null) {
-                        return Mono.error(new ImmutableMedicalSlotException(message.get()));
-                    }
-                    message.set("Medical slot has an active medical appointment attached to it.");
-                    if (slot.getMedicalAppointment() != null)  {
-                        return Mono.error(new ImmutableMedicalSlotException(message.get()));
-                    }
-                    slot.cancel();
-                    return medicalSlotRepository.save(slot);
+                .flatMap(medicalSlot -> {
+                    return onCanceledMedicalSlot(medicalSlot)
+                            .then(onImmutableMedicalSlot(medicalSlot))
+                            .then(Mono.defer(() -> {
+                                medicalSlot.cancel();
+                                return medicalSlotRepository.save(medicalSlot);
+                            }));
                 })
                 .then();
+    }
+
+    private Mono<Object> onCanceledMedicalSlot(MedicalSlot medicalSlot) {
+        String message = "Medical slot is already canceled.";
+        if (medicalSlot.getCanceledAt() != null) {
+            return Mono.error(new ImmutableMedicalSlotException(message));
+        }
+        return Mono.empty();
+    }
+
+    private Mono<Object> onImmutableMedicalSlot(MedicalSlot medicalSlot) {
+        String message = "Medical slot has an active medical appointment attached to it.";
+        if (medicalSlot.getMedicalAppointment() != null)  {
+            return Mono.error(new ImmutableMedicalSlotException(message));
+        }
+        return Mono.empty();
     }
 }
