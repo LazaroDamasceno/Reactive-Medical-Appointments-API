df35050cfc65e555ddd532ab85d57903354b755e
lazarodamasceno <lazarolawrence2@gmail.co>
2024-12-13 22:57:23 -0300

    Created the dto MedicalSlotResponseDto

---
+++ v1/src/main/java/com/api/v1/dtos/medical_slots/MedicalSlotResponseDto.java
@@ -0,0 +1,16 @@
+package com.api.v1.dtos.medical_slots;
+
+import com.api.v1.dtos.doctors.DoctorResponseDto;
+import com.api.v1.dtos.medical_appointments.MedicalAppointmentResponseDto;
+
+import java.time.ZoneId;
+
+public record MedicalSlotResponseDto(
+        DoctorResponseDto doctorResponseDto,
+        String availableAt,
+        ZoneId availableAtZone,
+        MedicalAppointmentResponseDto medicalAppointmentResponseDto,
+        String canceledAt,
+        ZoneId canceledAtZone
+) {
+}
