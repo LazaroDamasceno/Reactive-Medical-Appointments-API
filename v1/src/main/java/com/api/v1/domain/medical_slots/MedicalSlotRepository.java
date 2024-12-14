736dfbe620386b6f992c561d576b0e5e139c9d9c
lazarodamasceno <lazarolawrence2@gmail.co>
2024-12-13 22:16:39 -0300

    Fixed the repository MedicalSlotRepository

--- v1/src/main/java/com/api/v1/domain/medical_slots/MedicalSlotRepository.java
+++ v1/src/main/java/com/api/v1/domain/medical_slots/MedicalSlotRepository.java
@@ -3,5 +3,5 @@ package com.api.v1.domain.medical_slots;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
-public interface r extends ReactiveMongoRepository<MedicalSlot, ObjectId> {
+public interface MedicalSlotRepository extends ReactiveMongoRepository<MedicalSlot, ObjectId> {
 }
