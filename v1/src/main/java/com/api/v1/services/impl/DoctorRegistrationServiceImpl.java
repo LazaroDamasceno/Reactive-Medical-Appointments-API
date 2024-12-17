c4b40e0fffb41fa90416d9132e111ea24045f564
lazarodamasceno <lazarolawrence2@gmail.co>
2024-12-16 21:13:55 -0300

    Changed DoctorRegistrationServiceImpl

--- v1/src/main/java/com/api/v1/services/impl/DoctorRegistrationServiceImpl.java
+++ v1/src/main/java/com/api/v1/services/impl/DoctorRegistrationServiceImpl.java
@@ -1,5 +1,7 @@
 package com.api.v1.services.impl;
 
+import com.api.v1.domain.doctors.DoctorAuditTrail;
+import com.api.v1.domain.doctors.DoctorAuditTrailRepository;
 import com.api.v1.services.doctors.DoctorRegistrationService;
 import com.api.v1.domain.doctors.Doctor;
 import com.api.v1.domain.doctors.DoctorRepository;
@@ -12,17 +14,23 @@ import jakarta.validation.Valid;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+import java.util.Optional;
+
 @Service
 public class DoctorRegistrationServiceImpl implements DoctorRegistrationService {
 
     private final DoctorRepository doctorRepository;
     private final PersonRegistrationService personRegistrationService;
+    private final DoctorAuditTrailRepository doctorAuditTrailRepository;
 
     public DoctorRegistrationServiceImpl(
             DoctorRepository doctorRepository,
-            PersonRegistrationService personRegistrationService) {
+            PersonRegistrationService personRegistrationService,
+            DoctorAuditTrailRepository doctorAuditTrailRepository
+    ) {
         this.doctorRepository = doctorRepository;
         this.personRegistrationService = personRegistrationService;
+        this.doctorAuditTrailRepository = doctorAuditTrailRepository;
     }
 
     @Override
@@ -33,14 +41,39 @@ public class DoctorRegistrationServiceImpl implements DoctorRegistrationService 
                         .findByLicenseNumber(registrationDto.licenseNumberDto())
                         .singleOptional()
                         .flatMap(optional -> {
-                            if (optional.isPresent()) {
-                                return Mono.error(DuplicatedMedicalLicenseNumberException::new);
-                            }
-                            return Mono.defer(() -> {
-                               Doctor doctor = Doctor.create(registrationDto.licenseNumberDto(), foundPerson);
-                               return doctorRepository.save(doctor);
-                            });
+                            return handleDuplicatedMedicalLicenseNumber(optional)
+                                    .then(handleTerminatedDoctor(registrationDto.licenseNumberDto()))
+                                    .then(Mono.defer(() -> {
+                                        Doctor doctor = Doctor.create(registrationDto.licenseNumberDto(), foundPerson);
+                                        return doctorRepository.save(doctor);
+                                    }));
                 }))
                 .flatMap(savedDoctor -> Mono.just(DoctorResponseMapper.mapToDto(savedDoctor)));
+    }
+
+    private Mono<Void> handleDuplicatedMedicalLicenseNumber(Optional<Doctor> optional) {
+        if (optional.isPresent() && optional.get().getTerminatedAt() == null) {
+            return Mono.error(DuplicatedMedicalLicenseNumberException::new);
+        }
+        return Mono.empty();
+    }
+
+    public Mono<Void> handleTerminatedDoctor(String medicalLicenseNumber) {
+        return doctorRepository
+                .findAll()
+                .filter(doctor ->
+                    doctor.getTerminatedAt() != null
+                    && doctor.getLicenseNumber().equals(medicalLicenseNumber)
+                )
+                .singleOrEmpty()
+                .switchIfEmpty(Mono.empty())
+                .flatMap(doctor -> {
+                    DoctorAuditTrail doctorAuditTrail = DoctorAuditTrail.create(doctor);
+                    return doctorAuditTrailRepository
+                            .save(doctorAuditTrail)
+                            .then(Mono.defer(() -> {
+                               return doctorRepository.delete(doctor);
+                            }));
+                });
     }
 }
